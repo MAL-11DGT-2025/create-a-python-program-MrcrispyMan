bc2dc3f468da57c9c96ca81e43f324e161161ce1
MrcrispyMan <rhys.jenkins@marcellin.school.nz>
2025-03-07 12:54:48 +1300

    created a file and coded lists

---
+++ lists.py
@@ -0,0 +1,17 @@
+colours = ["red" , "blue" , "pruple" , "black" "green",] 
+print(colours[0])
+print(colours[1])
+print(colours[2])
+print(colours[3])
+print(colours[4])
+print(colours[5])
+
+for i in colours:
+    print(i)
+
+print(colours[-1])
+print(colours[-2])
+
+
+
+
